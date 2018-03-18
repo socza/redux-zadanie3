ce1f224b6403dd33fb1c99b7e8fcef8d16d817f6
Piotr Sociński <psocinski@gmail.com>
2018-03-18 02:00:51 +0100

    Finished task

--- src/comments.js
+++ src/comments.js
@@ -6,53 +6,57 @@ import {ADD_COMMENT,
 
 
 function reducer(state = [], action) {
-  switch(action.type) {
-    case ADD_COMMENT:
-      return  [
-        ...state,
-          {
-            id: action.id,
-            text: action.text,
-            votes: 0
-          }
-        ]
-    case EDIT_COMMENT:
-      return  state.map(comment => {
-          if (comment.id === action.id) {
-            return {
-              ...comment,
-              text: action.text
-            }
-          }
-          return comment;
-        });
+    switch(action.type) {
+        
+        case ADD_COMMENT:
+            return  [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    votes: 0
+                }
+            ]
+        
+        case EDIT_COMMENT:
+            return  state.map(comment => {
+                if (comment.id === action.id) {
+                    return {
+                        ...comment,
+                        text: action.text
+                    }
+                }
+                return comment;
+            });
 
-    case REMOVE_COMMENT:
-      return state.filter(comment => comment.id !== action.id)
-    case THUMB_UP_COMMENT:
-      return state.map(comment => {
-          if (comment.id === action.id) {
-            return {
-              ...comment,
-              votes: comment.votes + 1
-            }
-          }
-          return comment;
-        });
+        case REMOVE_COMMENT:
+            return state.filter(comment => comment.id !== action.id)
+       
+        case THUMB_UP_COMMENT:
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return {
+                        ...comment,
+                        votes: comment.votes + 1
+                    }
+                }
+                return comment;
+            });
 
-    case THUMB_DOWN_COMMENT:
-    return state.map(comment => {
-        if (comment.id === action.id) {
-          return {
-            ...comment,
-            votes: comment.votes - 1
-          }
-        }
-        return comment;
-      });
-    default:
-      return state;
-  }
+        case THUMB_DOWN_COMMENT:
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return {
+                        ...comment,
+                        votes: comment.votes - 1
+                    }
+                }
+                return comment;
+            });
+
+        default:
+            return state;
+    }
 }
 
 export default reducer;
